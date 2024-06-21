c361d485181b3b84263937dd73e97657cfdbe5c2
joshua <joshuapplg@gmail.com>
2024-06-21 15:38:52 +0700

    finish add

--- resources/views/components/layout/components/sidebar.blade.php
+++ resources/views/components/layout/components/sidebar.blade.php
@@ -99,6 +99,15 @@
                     <span class="nav-link-text ms-1">Testimonial</span>
                 </a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('contact*') ? 'active' : '' }}" href="{{ route('contacts.index') }}">
+                    <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="fas fa-address-book"></i> <!-- Corrected icon class -->
+                    </div>
+                    <span class="nav-link-text ms-1">Contact</span> <!-- Updated text to capitalize "Contact" -->
+                </a>
+            </li>
+            
 
         </ul>
     </div>
